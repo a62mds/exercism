57ee0a72b8ebf4daa62f8598158e6f0bdf0e3ff3
Mitch Sullivan <msullivan@krakenrobotics.com>
2019-09-09 00:34:41 -0230

    Added just enough code to build---first test fails

--- cpp/nth-prime/nth_prime.cpp
+++ cpp/nth-prime/nth_prime.cpp
@@ -1,5 +1,7 @@
 #include "nth_prime.h"
 
 namespace nth_prime {
-
+    int nth(int n) {
+        return 0;
+    }
 }  // namespace nth_prime
--- cpp/nth-prime/nth_prime.h
+++ cpp/nth-prime/nth_prime.h
@@ -2,7 +2,7 @@
 #define NTH_PRIME_H
 
 namespace nth_prime {
-
+    int nth(int n);
 }  // namespace nth_prime
 
 #endif // NTH_PRIME_H

6af8e9f4dd8049e128eb47e10d53e59d4dc17c81
Mitch Sullivan <msullivan@krakenrobotics.com>
2019-09-11 23:51:37 -0230

    Solved scrabble score

---
+++ cpp/scrabble-score/scrabble_score.h
@@ -0,0 +1,10 @@
+#if !defined(SCRABBLE_SCORE_H)
+#define SCRABBLE_SCORE_H
+
+#include <string>
+
+namespace scrabble_score {
+    unsigned score(std::string word);
+}  // namespace scrabble_score
+
+#endif // SCRABBLE_SCORE_H
